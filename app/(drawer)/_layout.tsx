fd7626b6fdf895c241a94d1d6adf1e06948d5e7a
N3MESI2 <tomasromerobaylac@gmail.com>
2026-08-12 02:05:05 -0300

    Angostar el drawer de amigos para que se vea como panel, no pantalla completa

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -2,7 +2,14 @@ import { Drawer } from 'expo-router/drawer';
 
 export default function DrawerLayout() {
   return (
-    <Drawer screenOptions={{ headerShown: false }}>
+    <Drawer
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'front',
+        drawerStyle: { width: '82%' },
+        overlayColor: 'rgba(0,0,0,0.4)',
+      }}
+    >
       <Drawer.Screen name="(tabs)" options={{ drawerLabel: 'Inicio' }} />
       <Drawer.Screen name="friends" options={{ drawerLabel: 'Amigos', title: 'Amigos' }} />
     </Drawer>
